998f4c8a782663b7f320113e722c4ea2c02c16bf
Hoang <duongminhhoang333@gmail.com>
2025-03-28 14:59:11 +0700

    create

---
+++ mobileApp_SkillBridge/src/main/java/com/_nguoidev/mobileApp_SkillBridge/MobileAppSkillBridgeApplication.java
@@ -0,0 +1,13 @@
+package com._nguoidev.mobileApp_SkillBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppSkillBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppSkillBridgeApplication.class, args);
+	}
+
+}
---
+++ mobileApp_SkillBridge/src/test/java/com/_nguoidev/mobileApp_SkillBridge/MobileAppSkillBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com._nguoidev.mobileApp_SkillBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppSkillBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
