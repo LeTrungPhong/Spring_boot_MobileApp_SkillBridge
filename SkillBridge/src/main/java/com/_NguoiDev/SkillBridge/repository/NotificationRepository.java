cea04d282d5ffb9375c7636dd7f867d3f3018498
Hoang <duongminhhoang333@gmail.com>
2025-05-15 12:10:16 +0700

    feat(notification): add get All MyNotification

--- SkillBridge/src/main/java/com/_NguoiDev/SkillBridge/repository/NotificationRepository.java
+++ SkillBridge/src/main/java/com/_NguoiDev/SkillBridge/repository/NotificationRepository.java
@@ -4,6 +4,8 @@ import com._NguoiDev.SkillBridge.entity.MyNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<MyNotification, Integer> {
 }
