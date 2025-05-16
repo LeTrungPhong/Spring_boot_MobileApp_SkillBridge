85a4621f4af9e0c787a61b5db2672fe8e25e9fa0
Trung Nguyễn <trung9204@gmail.com>
2025-05-16 10:06:13 +0700

    [Feat]: push grade submission request

---
+++ SkillBridge/src/main/java/com/_NguoiDev/SkillBridge/dto/request/GradeSubmissionRequest.java
@@ -0,0 +1,14 @@
+package com._NguoiDev.SkillBridge.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GradeSubmissionRequest {
+    private int point;
+} 
